c551da818b966fb0bda5d57077f97e7803db2b9c
HORIZON-Donut <kiettisak.k2546@gmail.com>
2025-04-04 19:08:10 +0700

    Complete function argument

--- src/core/datastructure.h
+++ src/core/datastructure.h
@@ -1,9 +1,9 @@
 #ifndef DATASTRUCTURE_H
 #define DATASTRUCTURE_H
 
-float* Array1D();
-float** Array2D();
-float*** Array3D();
+float* Array1D(int size);
+float** Array2D(int row, int col);
+float*** Array3D(int row, int col, int dep);
 
 float* Resize1D(float* arr, size_t n);
 float** Resize2D(float** arr, size_t n1, size_t n2);
