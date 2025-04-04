195ab39571fdd541758a35e1442e0fb5583c84f6
HORIZON-Donut <kiettisak.k2546@gmail.com>
2025-04-04 19:06:52 +0700

    Remove not yet to does stuff

--- src/core/datastructure/array.c
+++ src/core/datastructure/array.c
@@ -5,10 +5,3 @@ float* Array1D(int size)
 	float* arr = (float*)malloc(size);
 	return arr;
 }
-
-float** Array2D();
-float*** Array3D();
-
-float* Resize1D(float* arr, size_t n);
-float** Resize2D(float** arr, size_t n1, size_t n2);
-float*** Resize3D(float*** arr, size_t n1, size_t n2, size_t n3);
