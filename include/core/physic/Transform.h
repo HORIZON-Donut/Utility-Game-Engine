b296a2b6e96f5cfe6f860b43463532aad94d8695
HORIZON-Donut <kiettisak.k2546@gmail.com>
2025-05-21 20:53:24 +0700

    Defined transform struct

--- include/core/physic/Transform.h
+++ include/core/physic/Transform.h
@@ -1,4 +1,15 @@
 #ifndef INCLUDE_TRANSFORM_H
 #define INCLUDE_TRANSFORM_H
 
+#include "Vector.h"
+
+typedef struct Transform
+{
+    Vector3 position;
+    Vector3 localPosition;
+    Vector3 rotation;
+    Vector3 size;
+    Vector3 scale;
+}Transform;
+
 #endif
