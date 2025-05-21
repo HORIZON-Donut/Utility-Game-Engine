fcc0a68a3ee81e89776fdce997e6700f0dfa1dbe
HORIZON-Donut <kiettisak.k2546@gmail.com>
2025-05-21 21:27:08 +0700

    Add struct for collider

--- include/core/physic/Collider.h
+++ include/core/physic/Collider.h
@@ -1,6 +1,19 @@
 #ifndef INCLUDE_COLLISION_H
 #define INCLUDE_COLLISION_H
 
+#include "Vector.h"
+
 enum ColliderType { SPHERE, BOX, CAPSUALE};
+
+typedef struct Collider
+{
+    ColliderType type;
+
+    Vector3 offset;
+    Vector3 size;
+
+    double radius;
+    double height;
+}Collider;
 
 #endif
