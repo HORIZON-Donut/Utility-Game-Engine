ac57a575828115923b634e0025534647dea779e1
HORIZON-Donut <kiettisak.k2546@gmail.com>
2025-05-21 21:36:24 +0700

    Add physical body struct to body.

--- include/core/physic/Body.h
+++ include/core/physic/Body.h
@@ -1,4 +1,15 @@
 #ifndef INCLUDE_PHYSICBODY_H
 #define INCLUDE_PHYSICBODY_H
 
+#include "Transform.h"
+#include "RigidBody.h"
+#include "Collider.h"
+
+typedef struct PhysicalBody
+{
+    Transform* transform;
+    RigidBody* rigidBody;
+    Collider* collider;
+}PhysicalBody;
+
 #endif
