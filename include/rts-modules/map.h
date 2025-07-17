a2d8c4ecba34f687adc589858491cb2d4cf01b7d
HORIZON-Donut <kiettisak.k2546@gmail.com>
2025-07-17 09:36:44 +0700

    Define map node --------------- each node contain node id, coordiante, and other node data

--- include/rts-modules/map.h
+++ include/rts-modules/map.h
@@ -1,4 +1,18 @@
 #ifndef INCLUDE_MAP_H
 #define INCLUDE_MAP_H
 
+typedef struct Node
+{
+	unsigned int id;
+	int x;
+	int y;
+
+	int obstracle;
+	char obstracle_class;
+
+	int occupied;
+}Node;
+
+int loadMap(char* map);
+
 #endif
